77a15ff8c4c2a5b50eb800481c971e2fa7d8fde5
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-25 06:56:29 +0200

    text

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from './pages/Home';
 import About from './pages/About';
-import Navigation from './components/Navigation';
+// import Navigation from './components/Navigation';
 
 const App = () => {
   return (
